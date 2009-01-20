0eca818c1fd25cdd327213ace61a6a15b6c1346a
(no author) <(no author)@12f355fe-0486-481a-ad91-c297ab22b4e3>
2009-01-20 09:37:20 +0000

    Scripts added, in /etc.
    git-svn-id: https://svn.uvt.nl/ilk/trunk/sources/Mbmt@2452 12f355fe-0486-481a-ad91-c297ab22b4e3

---
+++ etc/mbmt.sh
@@ -0,0 +1,58 @@
+#! /bin/sh
+
+# Mbmt: Memory-based machine translation
+#
+# 
+#
+# mbmt.sh is a script that follows the default procedure to train
+# a translation model and a WOPR language model, and to translate
+# a test text.
+#
+# Usage: mbmt.sh training test
+#
+# training is a GIZA++ aligned A3.final file;
+# test is a source-language text, one sentence per line.
+#
+# Produces a translation of test in file test.out.
+# 
+# Assumes Timbl and Wopr installed;
+# see http://ilk.uvt.nl/timbl and http://ilk.uvt.nl/wopr
+
+if [ $# -ne 2 ]; then
+
+ echo Usage: mbmt.sh training test
+ echo " "
+ echo training is a GIZA++ aligned A3.final file;
+ echo test is a source-language text, one sentence per line.
+ echo " "
+
+else
+
+    # preparation 0: give heads up
+    echo preparing data...
+
+    # preparation 1: create training data for translation model
+    mbmt-0.1/mbmt-create-training $1 > $1.111.inst
+
+    # preparation 2: create training data for language model
+    mbmt-0.1/mbmt-tar-from-A3 $1
+
+    # preparation 3: WOPR creates language model
+    #                                       /dev/null 2>&1 &
+    wopr -s mbmtt.wopr -p filename:$2 >> wopr.out 2>&1 
+    wopr -s mbmts.wopr                >> wopr.out 2>&1 &
+
+    # preparation 4: test material is converted
+    mbmt-0.1/mbmt-create-test $2 > $2.111.inst
+
+    # training and testing
+    echo training and testing
+    Timbl -f $1.111.inst -I $1.111.inst.ibase -a1 +vdb+di +D -Beam=1 -t $2.111.inst -o $2.111.inst.out > /dev/null 2>&1
+
+    # decoding
+    mbmt-decode $2.111.inst.out > $2.out
+
+    # cleanup
+    rm -f $1.111.inst $2.111.inst $2.111.inst.out > /dev/null 2>&1
+
+fi
